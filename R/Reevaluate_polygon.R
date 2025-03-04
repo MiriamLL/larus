324ca58d945a08a08d16477f4818a6ae0f172ad6
Miriam Lerma <miriamjlerma@gmail.com>
2025-03-04 17:13:19 +0100

    update package

---
+++ R/Reevaluate_polygon.R
@@ -0,0 +1,19 @@
+#' Polygon of a buffer created from a central location
+#'
+#' Tracking data from one Yellow-footed gull tracked at La Paz, Mexico
+#' Includes 1 geometry polygon
+#'
+#' @docType data
+#'
+#' @usage data(Reevaluate_polygon)
+#'
+#' @format contains geometry of sfc_POLYGON
+#'
+#' @keywords datasets
+#'
+#' @references Lerma et al. (2025)
+#'
+#'
+#' @examples
+#' data(Reevaluate_polygon)
+"Reevaluate_polygon"
